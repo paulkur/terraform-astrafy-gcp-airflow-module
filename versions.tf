e9a45476f76cb66a735009281d4e70f85dab5364
paulkur <paulkurpis@gmail.com>
2023-12-29 14:37:12 +0000

    fix: airflow-2

--- versions.tf
+++ versions.tf
@@ -4,11 +4,14 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.74.0"
+	  version = ">= 4.74.0"
+      #version = "5.10.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.58.0"
+	  #version = ">= 4.58.0"
+      version = ">= 4.74.0"
+	  #version = "5.10.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
